76f99c2b5a553c495796669bcd6e148338a7368b
Paul Cowie <paul.cowie@ntlworld.com>
2021-09-18 17:51:15 +0100

    init

--- starmer_as_a_service/main.py
+++ starmer_as_a_service/main.py
@@ -15,7 +15,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 keiths = []
 
-with open('./keiths/keiths.csv') as csv_file:
+with open('../keiths/keiths.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
